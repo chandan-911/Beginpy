ca14eab7c71f08c10fb31e3cdbc4a77d44762bcc
chandan-911 <ck7464877@gmail.com>
2024-06-23 23:27:31 +0530

    while loops

---
+++ day17,18/loops.py
@@ -0,0 +1,53 @@
+# for numbers
+for i in range(1,20):
+    print(i)
+print("\n")   
+    
+# for characters
+for i in 'Chandan':
+    print(i)
+print("\n")    
+        
+# for colours
+colours=["red","green","yellow","blue"]        
+for colour in colours:
+    print(colour)
+    for i in colour:
+        print(i)
+print("\n") 
+       
+# step function 
+for i in range(5,25,2):
+    print(i)
+print("\n")
+
+# while loops
+i=0
+while(i<5):
+    print(i)
+    i=i+1
+print("done\n")    
+
+# conditionl
+i
+while(i<=20):
+    i=int(input("enter value"))
+    print(i)
+print("done\n")    
+
+# decrementing Loop 
+i=7
+while(i>0):
+    print(i)
+    i=i-1
+else:
+    print("else executed")    
+print("end\n")    
+
+
+# do while loop
+
+# do{
+#     # loop start / 
+# }while(condition):
+#     print(i)
--- day17/loops.py
+++
@@ -1,7 +0,0 @@
-for i in range(1,20):
-    print(i)
-    
-# for characters
-for i in 'Chandan':
-    print(i)
-        
